73a1e4fb67fd9fad033879d97924f8e9255db72a
rhurtado87 <ricardohurtado87@yahoo.com>
2024-01-24 20:00:26 -0800

    JavaScript

---
+++ script.js
@@ -0,0 +1,50 @@
+console.log("Script");
+
+var userName="Gergoe"; //creating and assinging value to the var
+
+userName= "jame" // chaning the var vale
+userName= "john"// 
+userName= "Rami"//
+userName= "Rick"//
+
+console.log(userName);
+
+var employeeName="Rick";
+var employeeAge="36";
+var employeeCountry="USA";
+var employeePhone="(555) 555-1234"
+var employeeSalaryMonthly= 20000;
+var employeeSalaryYearly= employeeSalaryMonthly *12; 
+
+document.write(
+    `
+    <h3>Employee Info</h3>
+    <p>Name: ${employeeName}</p>
+    <p>Age : ${employeeAge}</p>
+    <p>Country : ${employeeCountry}</p>
+    <p>Phone : ${employeePhone}</p>
+    <p>Salary Monthly: ${employeeSalaryMonthly}</p>
+    <p>Salary Yearly: ${employeeSalaryYearly}</p>
+    `
+);
+console.log(userName);
+
+var employeeName="john";
+var employeeAge="28";
+var employeeCountry="USA";
+var employeePhone="(555) 555-1235"
+var employeeSalaryMonthly= 10000;
+var employeeSalaryYearly= employeeSalaryMonthly *12; 
+
+document.write(
+    `
+    <h3>Employee Info</h3>
+    <p>Name: ${employeeName}</p>
+    <p>Age : ${employeeAge}</p>
+    <p>Country : ${employeeCountry}</p>
+    <p>Phone : ${employeePhone}</p>
+    <p>Salary Monthly: ${employeeSalaryMonthly}</p>
+    <p>Salary Yearly: ${employeeSalaryYearly}</p>
+    `
+);
+
